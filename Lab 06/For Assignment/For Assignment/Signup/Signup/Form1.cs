22b241662f0b5e78b988de89b1da9a0c571dc5cd
imac_m3_04 <imac_m3_04@imacm304.local>
2025-09-30 08:43:11 +0700

    Update assignment 6

--- Lab 06/For Assignment/For Assignment/Signup/Signup/Form1.cs
+++ Lab 06/For Assignment/For Assignment/Signup/Signup/Form1.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Windows.Forms;
-using ZstdSharp.Unsafe;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Signup
 {
     public partial class signup : Form
     {
-
-
         public signup()
         {
             InitializeComponent();
@@ -16,26 +12,22 @@ namespace Signup
 
         private void submit_Click(object sender, EventArgs e)
         {
-
-            string dateValue = birthDatePick.Value.ToString("yyyy-MM-dd");
-            info a1 = new info()
-            {
-                ID = 1,
-                FName = fname.Text,
-                LName = lname.Text,
-                Sex = sxCombo.Text,
-                Bdate = dateValue,
-                Email = email.Text,
-                Occup = occupation.Text,
-            };
             infoDAO infor = new infoDAO();
-            int result = infor.addOneRecord(a1); // Result will store returned value like 0 row affected.
-            // infor.addOneRecord(a1);
-            this.Hide();
-            userpage newuserpage = new userpage(infor.getAll());
-            newuserpage.ShowDialog();
 
-
+            // Check if user is trying to login (priority to login section)
+            if (!string.IsNullOrWhiteSpace(loginUsernameTextBox.Text) || !string.IsNullOrWhiteSpace(loginPasswordTextBox.Text))
+            {
+                HandleLogin(infor);
+            }
+            // Check if user is trying to signup
+            else if (!string.IsNullOrWhiteSpace(signupUsernameTextBox.Text) || !string.IsNullOrWhiteSpace(signupPasswordTextBox.Text))
+            {
+                HandleSignup(infor);
+            }
+            else
+            {
+                MessageBox.Show("Please fill all the fields in either login or signup section");
+            }
         }
 
         private void HandleLogin(infoDAO infor)
@@ -69,7 +61,7 @@ namespace Signup
 
         private void HandleSignup(infoDAO infor)
         {
-            // Validate personal information fields
+            // Validate personal information fields (REQUIREMENT A)
             if (string.IsNullOrWhiteSpace(fname.Text) || string.IsNullOrWhiteSpace(lname.Text) ||
                 string.IsNullOrWhiteSpace(sxCombo.Text) || string.IsNullOrWhiteSpace(email.Text) ||
                 string.IsNullOrWhiteSpace(occupation.Text))
@@ -78,7 +70,7 @@ namespace Signup
                 return;
             }
 
-            // Validate signup fields
+            // Validate signup fields (REQUIREMENT A)
             if (string.IsNullOrWhiteSpace(signupUsernameTextBox.Text) || string.IsNullOrWhiteSpace(signupPasswordTextBox.Text) ||
                 string.IsNullOrWhiteSpace(confirmPasswordTextBox.Text))
             {
@@ -86,11 +78,11 @@ namespace Signup
                 return;
             }
 
-            // Check password match
+            // Check password match (REQUIREMENT B) - MUST RETURN IF NOT MATCHING
             if (signupPasswordTextBox.Text != confirmPasswordTextBox.Text)
             {
                 MessageBox.Show("Your passwords do not match");
-                return;
+                return; // This STOPS the signup process
             }
 
             // Check for unique username
@@ -146,6 +138,10 @@ namespace Signup
             {
                 MessageBox.Show("Error creating account: " + ex.Message);
             }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
         }
     }
 }
--- Lab 06/For Assignment/For Assignment/Signup/Signup/infoDAO.cs
+++ Lab 06/For Assignment/For Assignment/Signup/Signup/infoDAO.cs
@@ -1,26 +1,13 @@
 ﻿using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Ocsp;
-using Org.BouncyCastle.Utilities.Encoders;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
-using System.Xml.Linq;
-using System.Configuration;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Signup
 {
     internal class infoDAO
     {
         string connectionString = "datasource=localhost;port=3306;username=root;password=root;database=test;";
-        // Create a connectionString specifying datasource, port, username, password and database
-        List<info> info_try = new List<info>();
-        // Function to add new record
 
         // Check if username already exists
         internal bool IsUsernameExists(string username)
@@ -48,10 +35,10 @@ namespace Signup
             }
         }
 
+        // Function to add new record and return the inserted ID
         internal int addOneRecord(info a1)
         {
             MySqlConnection connect = new MySqlConnection(connectionString);
-
             connect.Open();
 
             MySqlCommand cmd = new MySqlCommand("INSERT INTO `signup`(`First_Name`, `Last_Name`, `Sex`, `BirthDate`, `Email`, `Occupation`) VALUES(@fname, @lname, @sex, @birthdate, @email, @occupation)", connect);
@@ -60,10 +47,12 @@ namespace Signup
             cmd.Parameters.AddWithValue("@sex", a1.Sex);
             cmd.Parameters.AddWithValue("@birthdate", a1.Bdate);
             cmd.Parameters.AddWithValue("@email", a1.Email);
-            cmd.Parameters.AddWithValue("@Occupation", a1.Occup);
-            int newRows = cmd.ExecuteNonQuery();
+            cmd.Parameters.AddWithValue("@occupation", a1.Occup);
+
+            cmd.ExecuteNonQuery();
+            int newId = (int)cmd.LastInsertedId;
             connect.Close();
-            return newRows;
+            return newId;
         }
 
         // Add login record
@@ -128,27 +117,19 @@ namespace Signup
             return null;
         }
 
-        // Function to retrieve information
+        // Function to retrieve all information (keep for compatibility)
         public List<info> getAll()
         {
             List<info> returnAll = new List<info>();
-            // Create a list of object of class info
             MySqlConnection conn = new MySqlConnection(connectionString);
             conn.Open();
 
-            // Create MySqlConnection object and open connection
             MySqlCommand cmd = new MySqlCommand("SELECT * FROM signup", conn);
 
-            // Create MySqlCommand object which read all the records in database
-
-
-            // Create MySqlDataReader object and read all the data gained from  MySqlCommand object. Pass the data to relevant info class parameter
             using (MySqlDataReader reader = cmd.ExecuteReader())
             {
                 while (reader.Read())
                 {
-                    // Create a info class object and pass info to relevant variable
-                    //eg for string reader.GetString and for integer reader.GetInt32
                     info r = new info
                     {
                         ID = reader.GetInt32(0),
@@ -159,16 +140,11 @@ namespace Signup
                         Email = reader.GetString(5),
                         Occup = reader.GetString(6),
                     };
-
-                    //Add all information to info class object
                     returnAll.Add(r);
                 }
             }
-            // Close connection
-            conn.Close ();
-            // Return all records
+            conn.Close();
             return returnAll;
-           
         }
 
         // Function to update one record
@@ -176,31 +152,29 @@ namespace Signup
         {
             MySqlConnection conn = new MySqlConnection(connectionString);
             conn.Open();
-            MySqlCommand cmd = new MySqlCommand("UPDATE signup SET First_Name = @fname, Last_Name = @lname, Sex = @sex, BirthDate = @birthdate, Email = @email, Occupation = @Occupation WHERE ID = @ID", conn);
+            MySqlCommand cmd = new MySqlCommand("UPDATE signup SET First_Name = @fname, Last_Name = @lname, Sex = @sex, BirthDate = @birthdate, Email = @email, Occupation = @occupation WHERE ID = @ID", conn);
             cmd.Parameters.AddWithValue("@ID", a1.ID);
             cmd.Parameters.AddWithValue("@fname", a1.FName);
             cmd.Parameters.AddWithValue("@lname", a1.LName);
             cmd.Parameters.AddWithValue("@sex", a1.Sex);
             cmd.Parameters.AddWithValue("@birthdate", a1.Bdate);
             cmd.Parameters.AddWithValue("@email", a1.Email);
-            cmd.Parameters.AddWithValue("@Occupation", a1.Occup);
+            cmd.Parameters.AddWithValue("@occupation", a1.Occup);
             int newRows = cmd.ExecuteNonQuery();
             conn.Close();
             return newRows;
         }
 
-        // Function to delete one record
+        // Function to delete one record (CASCADE will handle login table)
         internal int deleteOneRecord(int id_row)
         {
             MySqlConnection connect = new MySqlConnection(connectionString);
             connect.Open();
-            MySqlCommand cmd = new MySqlCommand("DELETE FROM `signup` WHERE `signup`.`ID` =@ID",
-            connect);
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM `signup` WHERE `signup`.`ID` = @ID", connect);
             cmd.Parameters.AddWithValue("@ID", id_row);
             int newRows = cmd.ExecuteNonQuery();
             connect.Close();
             return newRows;
         }
-       
     }
 }
